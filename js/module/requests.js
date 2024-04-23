7b1c58dba49914046e0fd577fdc0d5f0622c3c76
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-04-23 18:00:28 -0500

    feat: :sparkles: Ejercicio 7
    Imprimir los distintos estados de un pedido

--- js/module/requests.js
+++ js/module/requests.js
@@ -2,11 +2,10 @@
 export const getAllStatus = async()=>{
     let res = await fetch("http://localhost:5508/requests")
     let data =await res.json();
-    let dataUpdate = [];
+    let dataUpdate = new Set(); //Se garantiza que no hayan datos duplicados
     data.forEach(val =>{
-        dataUpdate.push({
-            estado : val.status
-        })
+        let {status} = val
+        dataUpdate.add(status)
     })
     return dataUpdate;
 }
