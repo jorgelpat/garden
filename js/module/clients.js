f51e3070edc143d4aa3d836dd92d26971ac182f5
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-04-23 18:02:01 -0500

    chore: :bulb: comentario de ejercicio 8

--- js/module/clients.js
+++ js/module/clients.js
@@ -12,3 +12,11 @@ export const getAllSpainClients = async()=>{
     })
     return dataUpdate;
 }
+
+// Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos. Resuelva la consulta:
+
+// Utilizando la función YEAR de MySQL.
+// Utilizando la función DATE_FORMAT de MySQL.
+// Sin utilizar ninguna de las funciones anteriores.
+
+
