5d3c472785825fa6694c1a41652ce3adbf5fa12f
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-04-25 23:50:00 -0500

    refactor: :recycle: Se comenta código para mantenerse rápidamente disponible

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 import {getAllOfficesCodeAndCity,getAllOficesFromSpainCityAndMovil}from "./module/offices.js"
 import {getAllEmployeesWithBossAndCodeSeven,getBossFullNameAndEmail,getAllFullNameNoSale} from "./module/employees.js";
-import {getAllSpainClients,getAllMadridClients} from "./module/clients.js";
+import {getAllSpainClients,getAllMadridClients,getClientsAndEmployeesNames} from "./module/clients.js";
 import {getAllStatus,getAllLateOrder,getAllRequestTwoDaysBefore,getAllRejectedOrder,getAllOrdersDeliveredInJanuary} from "./module/requests.js";
 import {getAllPaymentsbyYear,getAllPymentType,getAllPayment} from "./module/payments.js";
 import {getAllGammaOrnamentalesProduct} from "./module/products.js"
 
-
-console.log(await getAllMadridClients());
+// console.log(await getAllOfficesCodeAndCity());
+// console.log(await getAllOficesFromSpainCityAndMovil());
+// console.log(await getAllEmployeesWithBossAndCodeSeven());
+// console.log(await getBossFullNameAndEmail());
+// console.log(await getAllFullNameNoSale());
+// console.log(await getAllSpainClients());
+// console.log(await getAllMadridClients());
+// console.log(await getClientsAndEmployeesNames());  Ejercicio 17
+// console.log(await getAllStatus());
+// console.log(await getAllLateOrder());
+// console.log(await getAllRequestTwoDaysBefore());
+// console.log(await getAllRejectedOrder(2009));
+// console.log(await getAllOrdersDeliveredInJanuary());
+// console.log(await getAllPaymentsbyYear(2008));
+// console.log(await getAllPymentType(2008));
+// console.log(await getAllPayment());
+// console.log(await getAllGammaOrnamentalesProduct());
