88dfd919f5a2f9e0e394598eb6a5f4f827f6ed1c
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-04-25 23:20:39 -0500

    feat: :sparkles: Ejercicio 16
    Devuelve un listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el código de empleado 11 o 30.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import {getAllOfficesCodeAndCity,getAllOficesFromSpainCityAndMovil}from "./module/offices.js"
 import {getAllEmployeesWithBossAndCodeSeven,getBossFullNameAndEmail,getAllFullNameNoSale} from "./module/employees.js";
-import {getAllSpainClients} from "./module/clients.js";
+import {getAllSpainClients,getAllMadridClients} from "./module/clients.js";
 import {getAllStatus,getAllLateOrder,getAllRequestTwoDaysBefore,getAllRejectedOrder,getAllOrdersDeliveredInJanuary} from "./module/requests.js";
 import {getAllPaymentsbyYear,getAllPymentType,getAllPayment} from "./module/payments.js";
 import {getAllGammaOrnamentalesProduct} from "./module/products.js"
 
 
-console.log(await getAllGammaOrnamentalesProduct());
+console.log(await getAllMadridClients());
--- js/module/clients.js
+++ js/module/clients.js
@@ -13,6 +13,22 @@ export const getAllSpainClients = async()=>{
     return dataUpdate;
 }
 
+// Devuelve un listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el código de empleado 11 o 30.
 
+export const getAllMadridClients = async()=>{
+    let res = await fetch("http://localhost:5501/clients?city=Madrid")
+    let data =await res.json();
+    let dataUpdate = [];
+    data.forEach(val =>{
+        if (val.code_employee_sales_manager == 11 || val.code_employee_sales_manager == 30){
+            dataUpdate.push({
+                Nombre : val.client_name,
+                Ciudad : val.city,
+                Representante_de_Ventas: val.code_employee_sales_manager
+            })
+        }  
+    })
+    return dataUpdate;
+}
 
 
